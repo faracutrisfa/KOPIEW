61dd34e6aa7094a8deef884d60d8cbcc94e2707b
REMITMID <sanders060806@gmail.com>
2025-11-26 09:27:22 +0700

    feat: perbaikan wishlist

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -84,12 +84,16 @@ const handleLogout = () => {
             </svg>
           </div>
 
-          <button class="text-text-body hover:text-primary transition-colors">
-            <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-            </svg>
-          </button>
+          <RouterLink 
+              :to="{ name: 'wishlist' }" 
+              class="text-text-body hover:text-primary transition-colors"
+              aria-label="Kunjungi Wishlist"
+          >
+              <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+              </svg>
+          </RouterLink>
 
           <div class="relative">
             <button @click="showProfileDropdown = !showProfileDropdown"
