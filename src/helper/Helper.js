fb7a911c659311513c1d56482560cb0947da8983
rawhx_x <achmadhasbil24@gmail.com>
2025-11-26 08:10:09 +0700

    fix: Update normalizeImageUrl function to handle additional URL formats

--- src/helper/Helper.js
+++ src/helper/Helper.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+import { API_URL } from "../services/api";
 
 export default function normalizeImageUrl(url) {
     if (url && (url.startsWith("http://") || url.startsWith("https://"))) {
@@ -7,6 +7,10 @@ export default function normalizeImageUrl(url) {
 
     if (url && url.startsWith("/storage")) {
         return `${API_URL}${url}`;
+    }
+
+    if (url && url.startsWith("photos")) {
+        return `${API_URL}/storage/${url}`;
     }
 
     return "https://blogger.googleusercontent.com/img/a/AVvXsEgFEMze_nmuI79pIvhZ6Nam0oSOUrWz0jTW-WZmhkrfb0gtvuAbTyWcRrDmfBTwDq6nhFSziH9LwV1zD2pkAsK2AmFaiqXlPl4Wp8gLMN5sQ0y0ajM_PeXaFl5DZYU1p6oNdwLR84hqMbPF9z8n_HUGVQAxL0OT4vH7S21SnzZ_GgFaGmyI-lklxLeJ";
